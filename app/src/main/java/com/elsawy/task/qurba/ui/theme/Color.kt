25d7a4f3954933f3a87f186ba07d5a8ca292cc26
Ahmed7Elsawy <ahmedelsawy487@gmail.com>
2022-03-07 16:31:47 +0200

    initial commit

---
+++ app/src/main/java/com/elsawy/task/qurba/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.elsawy.task.qurba.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val white = Color(0xFFE5E5E5)
+val purple400 = Color(0xE64E60EB)
+val purple300 = Color(0xE6C63AD6)
+val purple100 = Color(0xE6C13BD8)
