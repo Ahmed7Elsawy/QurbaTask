934adf4b8b021f39d2d6a526f47c68c29af24769
Ahmed7Elsawy <ahmedelsawy487@gmail.com>
2022-03-09 23:25:59 +0200

    add data package

--- app/src/main/java/com/elsawy/task/qurba/components/PostCaption.kt
+++ app/src/main/java/com/elsawy/task/qurba/components/PostCaption.kt
@@ -5,17 +5,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.elsawy.task.qurba.ui.theme.Black
 
 @Composable
-fun PostCaption(caption: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eros est, blandit eu nunc sit amet") {
+fun PostCaption(
+   caption: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eros est, blandit eu nunc sit amet",
+   horizontalMargin: Dp = 16.dp,
+) {
    Text(
       text = caption,
       fontSize = 12.sp,
       style = MaterialTheme.typography.body1,
       color = Black,
-      modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+      modifier = Modifier.padding(horizontal = horizontalMargin, vertical = 8.dp)
    )
 }
